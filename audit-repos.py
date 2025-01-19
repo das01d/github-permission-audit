1a97fae710fe03c934ab189e131a09ab5b8a5e80
Dennis Somerville <dasomerville@sherwin.com>
2025-01-19 12:03:04 -0500

    Refactor audit-repos.py to improve API request headers and enhance data handling; add openpyxl to requirements

--- audit-repos.py
+++ audit-repos.py
@@ -8,16 +8,17 @@ load_dotenv()
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 org = "sherwin-williams-co"
 
-# Get list of repos that have the correct topic
-headers = {"Authorization": f"token {GITHUB_TOKEN}"}
-search_url = f"https://api.github.com/search/repositories?per_page=100&q=topic:business-platforms&org:{org}"
-repos = requests.get(search_url, headers=headers).json().get('items', [])
+headers = {
+    "Authorization": f"token {GITHUB_TOKEN}",
+    "Accept": "application/vnd.github.v3+json"
+}
 
-data_dict = pd.DataFrame(repos)
-ds_list_of_repos_from_topic = data_dict.loc[:,'name']
+# Get list of repos that have the correct topic
+search_url = f"https://api.github.com/search/repositories?q=topic:business-platforms+org:{org}&per_page=100"
+repos = requests.get(search_url, headers=headers).json().get('items', [])
+ds_list_of_repos_from_topic = pd.DataFrame(repos).loc[:,'name']
 
 # Get list of repos from our groups that should have access
-
 def get_team_repos(team_slug):
     repos_url = f"https://api.github.com/orgs/{org}/teams/{team_slug}/repos?per_page=100"
     response = requests.get(repos_url, headers=headers)
@@ -25,7 +26,6 @@ def get_team_repos(team_slug):
         return response.json()
     else:
         raise(f"Error: {response.status_code}") 
-
 
 resp_admins = get_team_repos(team_slug = "gg-aad-dss-digitalit-iom-gatekeepers")
 resp_devs = get_team_repos(team_slug = "gg-aad-dss-digitalit-iom-developers")
@@ -37,7 +37,7 @@ def shape_data(resp):
     temp_data_frame['has_admin_permission'] = temp_data_frame['permissions'].apply(lambda x: x['admin'])
 
     # reshape to only include the rows needed
-    temp_data_frame = pd.DataFrame(temp_data_frame.loc[:,['name','has_admin_permission']])
+    temp_data_frame = pd.DataFrame(temp_data_frame.loc[:,['name','has_admin_permission', 'topics']])
     return temp_data_frame
 
 ds_admin_repos = shape_data(resp_admins)
@@ -46,8 +46,8 @@ ds_dev_repos = shape_data(resp_devs)
 # Get a unique list of all the repos
 ds_all_repos = pd.concat([ds_admin_repos, ds_dev_repos, ds_list_of_repos_from_topic], axis=0)
 
-# keep only the name column
-ds_all_repos = pd.DataFrame(ds_all_repos.loc[:,'name'])
+# keep only the name and topics column
+ds_all_repos = pd.DataFrame(ds_all_repos.loc[:,['name', 'topics']])
 
 # remove duplicates
 ds_all_repos = ds_all_repos.drop_duplicates(subset='name').reset_index(drop=True)
@@ -55,8 +55,9 @@ ds_all_repos = ds_all_repos.drop_duplicates(subset='name').reset_index(drop=True
 # Copy all_repos into a new "report" dataframe
 ds_report = ds_all_repos.copy()
 
+## Report on the topic being present
+ds_report['topic-present'] = False
 for r in ds_list_of_repos_from_topic:
-    # lookup name ds_report name column and update 'topic-present' to true if it exists
     ds_report.loc[ds_report['name'] == r, 'topic-present'] = True
 
 # loop through ds_admin_repos.  update the column 'has-admin-team-present' to true in the ds_report data frame where the repo name matches
@@ -64,7 +65,8 @@ for r in ds_dev_repos['name' ]:
     # lookup name ds_report name column and update 'dev-team-present' to true if it exists
     ds_report.loc[ds_report['name'] == r, 'dev-team-present'] = True
     # update 'dev-team-has-admin-permission' to true if the repo has admin permission
-    ds_report.loc[ds_report['name'] == r, 'dev-team-has-admin-permission'] = ds_dev_repos.loc[ds_dev_repos['name'] == r, 'has_admin_permission'].values[0]
+    # set the value to the opposite of values[0], assuming it is boolean
+    ds_report.loc[ds_report['name'] == r, 'dev-team-does-not-have-admin-permission'] = not ds_dev_repos.loc[ds_dev_repos['name'] == r, 'has_admin_permission'].values[0]
     
 for r in ds_admin_repos['name' ]:
     # lookup name ds_report name column and update 'dev-team-present' to true if it exists
