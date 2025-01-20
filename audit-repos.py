ac85351b7ec7f8cb816a182911dd38f2cdd57c05
Dennis Somerville <dasomerville@sherwin.com>
2025-01-20 11:47:44 -0500

    Including 'clone_url' and 'archived' fields in report

--- audit-repos.py
+++ audit-repos.py
@@ -16,7 +16,7 @@ headers = {
 # Get list of repos that have the correct topic
 search_url = f"https://api.github.com/search/repositories?q=topic:business-platforms+org:{org}&per_page=100"
 repos = requests.get(search_url, headers=headers).json().get('items', [])
-ds_list_of_repos_from_topic = pd.DataFrame(repos).loc[:,'name']
+ds_list_of_repos_from_topic = pd.DataFrame(repos).loc[:,['name', 'clone_url', 'archived']]
 
 # Get list of repos from our groups that should have access
 def get_team_repos(team_slug):
@@ -37,7 +37,7 @@ def shape_data(resp):
     temp_data_frame['has_admin_permission'] = temp_data_frame['permissions'].apply(lambda x: x['admin'])
 
     # reshape to only include the rows needed
-    temp_data_frame = pd.DataFrame(temp_data_frame.loc[:,['name','has_admin_permission', 'topics']])
+    temp_data_frame = pd.DataFrame(temp_data_frame.loc[:,['name','has_admin_permission', 'topics', 'clone_url', 'archived']])
     return temp_data_frame
 
 ds_admin_repos = shape_data(resp_admins)
@@ -47,7 +47,7 @@ ds_dev_repos = shape_data(resp_devs)
 ds_all_repos = pd.concat([ds_admin_repos, ds_dev_repos, ds_list_of_repos_from_topic], axis=0)
 
 # keep only the name and topics column
-ds_all_repos = pd.DataFrame(ds_all_repos.loc[:,['name', 'topics']])
+ds_all_repos = pd.DataFrame(ds_all_repos.loc[:,['name', 'topics', 'clone_url', 'archived']])
 
 # remove duplicates
 ds_all_repos = ds_all_repos.drop_duplicates(subset='name').reset_index(drop=True)
@@ -57,18 +57,18 @@ ds_report = ds_all_repos.copy()
 
 ## Report on the topic being present
 ds_report['topic-present'] = False
-for r in ds_list_of_repos_from_topic:
+for r in ds_list_of_repos_from_topic['name']:
     ds_report.loc[ds_report['name'] == r, 'topic-present'] = True
 
 # loop through ds_admin_repos.  update the column 'has-admin-team-present' to true in the ds_report data frame where the repo name matches
-for r in ds_dev_repos['name' ]:
+for r in ds_dev_repos['name']:
     # lookup name ds_report name column and update 'dev-team-present' to true if it exists
     ds_report.loc[ds_report['name'] == r, 'dev-team-present'] = True
     # update 'dev-team-has-admin-permission' to true if the repo has admin permission
     # set the value to the opposite of values[0], assuming it is boolean
     ds_report.loc[ds_report['name'] == r, 'dev-team-does-not-have-admin-permission'] = not ds_dev_repos.loc[ds_dev_repos['name'] == r, 'has_admin_permission'].values[0]
     
-for r in ds_admin_repos['name' ]:
+for r in ds_admin_repos['name']:
     # lookup name ds_report name column and update 'dev-team-present' to true if it exists
     ds_report.loc[ds_report['name'] == r, 'gatekeeper-team-present'] = True
     # update 'dev-team-has-admin-permission' to true if the repo has admin permission
